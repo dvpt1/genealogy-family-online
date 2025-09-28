917d0b0acc7b43db97e3fe2a068f909d47e53795
Dmitriy <dvpt@narod.ru>
2025-09-28 22:10:45 +0500

    20250928

--- csavec.php
+++ csavec.php
@@ -13,14 +13,69 @@ if($user_data == 0) {
   exit;
 }
 
-echo $_POST['username']."<br>";
-echo $_POST['password']."<br>";
+//echo $_POST['username']."<br>";
+//echo $_POST['password']."<br>";
 echo $_POST['persona']."<br>";
-echo $_POST['birth']."<br>";
-echo $_POST['death']."<br>";
+//echo $_POST['birth']."<br>";
+//echo $_POST['death']."<br>";
 
-//$mainPath = __DIR__ ;//. '/cardfile/';
-//$files = glob($mainPath."/cards/*.card");
-//print_r($files);
+  if($_POST['id'] == 0){
+    $personnew = $persons[count($persons) - 1];
+    $new_inx = $personnew[$fldINX] + 1;
+    $new_id = $personnew[$fldID] + 1;
+
+    $inx_person = $new_inx;
+    $id_person = $new_id;
+  } else {
+    $inx_person = $_POST['inx'];;
+    $id_person = $_POST['id'];
+  }
+
+/* create json */
+  $jsonPerson = new stdClass(); 
+  $jsonPerson->id = intval($id_person);
+  $jsonPerson->gender = $_POST['genders'];
+  $jsonPerson->person = $_POST['persona'];
+  $jsonPerson->birthday->date = $_POST['birth'];
+  $jsonPerson->birthday->place = $_POST['placeb'];
+  $jsonPerson->birthday->maps = $_POST['mapsb'];
+  $jsonPerson->deathday->date = $_POST['death'];
+  $jsonPerson->deathday->place = $_POST['placed'];
+  $jsonPerson->deathday->maps = $_POST['mapsd'];
+  $jsonPerson->lifeday->date = $_POST['live'];
+  $jsonPerson->lifeday->place = $_POST['placel'];
+  $jsonPerson->lifeday->maps = $_POST['mapsl'];
+  $jsonPerson->burialday->date = $_POST['burial'];
+  $jsonPerson->burialday->place = $_POST['placet'];
+  $jsonPerson->burialday->maps = $_POST['mapst'];
+/***/
+  
+/***/
+  $jsonPerson->occupation = $_POST['occu'];
+  $jsonPerson->national = $_POST['nati'];
+  $jsonPerson->education = $_POST['educ'];
+  $jsonPerson->religion = $_POST['reli'];
+  $jsonPerson->notes = $_POST['notes'];
+  $jsonPerson->icon = $_POST['icon'];
+  
+  $timestamp = date('YmdHisu');
+  $jsonPerson->stamp->timestamp = $timestamp;
+  $jsonPerson->stamp->avtor = $avtora;
+  $jsonPerson->stamp->datetime = $datetimea;
+  $jsonPerson->stamp->avtorup = $avtorupa;
+  $jsonPerson->stamp->datetimeup = $datetimeupa;
+
+  $jsonPersonvar = json_encode($jsonPerson);
+ 
+  // Generate json file
+  $number = str_pad($id_person, 6, '0', STR_PAD_LEFT); // "000001"
+  $file = __DIR__ ."/cards/$number.card";
+  file_put_contents($file, $jsonPersonvar);
+//echo $number.":".$file;
+
+////////////////////////////////////////////////////////////
+  //$timestamp = date('YmdHisu');
+  file_put_contents("timestamp", $timestamp);
+////////////////////////////////////////////////////////////
 
 ?>
--- csub.php
+++ csub.php
@@ -15,6 +15,10 @@ mysql_query('SET NAMES utf8');
 
 function _check_database($user, $pass)
 {
+  //GLOBAL $pepper;// = getConfigVariable("pepper");
+  //$pwd_peppered = hash_hmac("sha256", $pass, $pepper);
+  //$pwd_hashed = password_hash($pwd_peppered, PASSWORD_DEFAULT);
+
   $Q = mysql_query(" SELECT id FROM cusers WHERE name = '$user' AND pass = '$pass' ");
   if(mysql_num_rows($Q) == 0) return 0;
   else return mysql_fetch_array($Q);
@@ -97,6 +101,11 @@ function _check_datauserid($id)
 
 function _adduser_database($user, $pass, $fio, $country, $postcode, $city, $address, $phone, $http, $notes, $activation, $acces)
 {
+  //GLOBAL $pepper;// = getConfigVariable("pepper");
+  //$pwd_peppered = hash_hmac("sha256", $pass, $pepper);
+  //$pwd_hashed = password_hash($pwd_peppered, PASSWORD_DEFAULT); //$pwd_hashed = password_hash($pwd_peppered, PASSWORD_ARGON2ID);
+
+//echo $user.$pwd_peppered.$fio.$country.$postcode.$city.$address.$phone.$http.$notes.$activation."<br>"; 
   $Q = mysql_query("INSERT INTO cusers (name,pass,fio,country,postcode,city,address,phone,http,notes,activation,acces) VALUES ('$user','$pass','$fio','$country','$postcode','$city','$address','$phone','$http','$notes','$activation','$acces')");
 //echo "Q: ".$Q."<br>"; 
 }
