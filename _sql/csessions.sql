2329eecf4141235492df880611822b255e9db029
Dmitriy <dvpt@narod.ru>
2025-11-11 12:01:29 +0500

    20251111 12:01

--- _sql/csessions.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `csessions` (
-  `session` varchar(32) NOT NULL default '',
-  `id` int(11) NOT NULL default '0',
-  `name` varchar(80) NOT NULL default '',
-  PRIMARY KEY  (`session`)
-) ENGINE=MyISAM;
